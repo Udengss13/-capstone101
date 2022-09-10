0013f140bb6751910bfde7917b3e7eea2c534428
Udengss13 <melody13santiago@gmail.com>
2022-09-10 13:24:31 +0800

    Message

--- admin-quicktips.php
+++ admin-quicktips.php
@@ -1,9 +1,10 @@
 <?php
     require_once "php/user-list-process.php";
     require('php/connection.php');
+    
 
     
-    $queryimage = "SELECT * FROM admin_content_homepage"; //You don't need a like you do in SQL;
+    $queryimage = "SELECT * FROM admin_quicktips"; //You don't need a like you do in SQL;
     $resultimage = mysqli_query($db_admin_account, $queryimage);
     
 ?>
@@ -62,11 +63,92 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link c-green" href="admin-login.php">Logout</a>
+          <a class="nav-link c-green" href="admin-login.php">Loginn</a>
         </li>
       </ul>
     </div>
   </nav>
+
+  <!--All Content for Image Here-->
+  <div class=" px-3 mt-3 mb-3">
+        <h4 class="text-center c-white py-3">Image Content for Home</h4>
+
+        <form action="php/quicktips-image-process.php" method="post" enctype="multipart/form-data"
+            class="row gap-2 justify-content-center">
+            <div class="row justify-content-md-center mb-5">
+                <div class="col-lg-7 col-md-6 col-sm-12">
+                    <div class="card d-flex justify-content-center">
+                        <div class="card-header">
+                            Upload New Image for Quicktips
+
+                            <?php if(!empty($messages)){
+	                              echo "<div class='alert alert-success'>";
+	                                foreach ($messages as $message) {
+		                              echo "<span class='glyphicon glyphicon-ok'></span>&nbsp;".$message."<br>";
+	                                }
+	                                 echo "</div>";
+                                    }
+                          ?>
+
+                            <ul class="list-group list-group-flush">
+                                
+                        
+                                <!--Body-->
+                                <li class="list-group-item">
+                                    <div> <label>Information:</label></div>
+                                    <textarea name="info" style="height:150px;" required class="col-12"
+                                        placeholder="Information"></textarea>
+                                </li>
+                                <!--Choose File-->
+                                <li class="list-group-item">
+                                    <input name="photo" class="" id="upload-news" type="file" required>
+                                </li>
+
+                                <li class="list-group-item">
+                                    <!--Add button-->
+                                    <button type="submit" name="upload_image_content"
+                                        class="btn btn-outline-success float-end" style="max-width:450px;">Add</button>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+        </form>
+
+        <!--Displaying data in table-->
+        <div class="div_background_light">
+            <div class="table-responsive mt-4 mx-auto" style="width:95%;">
+                <table class="table mt-3">
+                    <thead class="table-dark c-white">
+                        <th>Image ID</th>
+                        <th>Information</th>
+                        <th>Delete</th>
+                    </thead>
+                    <?php while($rowimage =  mysqli_fetch_array($resultimage)){ ?>
+                    <tbody>
+                        <td class="text-nowrap c-white"><?php echo $rowimage['id']; ?></td>
+                        <td class="text-nowrap c-white"><?php echo $rowimage['info']; ?></td>
+
+                        <td class="c-red d-flex mt-1">
+
+                            <a href="quicktips-edit-content.php?updateid=<?php echo $rowimage['id'];?>">
+                                <span class="btn btn-outline-success mx-2">Edit </span>
+                            </a>
+
+                            <a href="php/quicktips-image-process.php?id=<?php echo $rowimage['id'];?>"><input
+                                    type="button" class="btn btn-outline-danger" value="Delete"></a>
+                        </td>
+                    </tbody>
+                    <?php } ?>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+
+  
 
  
 
