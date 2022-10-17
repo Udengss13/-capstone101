cf2d0eeabe89568385b4c42142b347e3d274b18a
Udengss13 <melody13santiago@gmail.com>
2022-10-17 10:24:50 +0800

    Mssage

--- admin-content.php
+++ admin-content.php
@@ -31,7 +31,7 @@ $result = $db_admin_account->query("SELECT image_path from admin_carousel_homepa
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="css/admin.css">
+<link rel="stylesheet" type="text/css" href="css/admin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
--- index.php
+++ index.php
@@ -63,7 +63,7 @@
                 </div>
                 <div class="text-nowrap">
                     <li class="nav-item">
-                        <a class="nav-link text-white" href="aboutUs.php">ABOUT US</a>
+                        <a class="nav-link text-white" href="#about">ABOUT US</a>
                     </li>
                 </div>
                 <div class="text-nowrap">
@@ -191,6 +191,22 @@
         </section>
     </section>
 
+    <!-- About us -->
+    <section class="flex-sect" id="about" style="background-color:#9FBACD;">>
+        <section id="imagesection" class="div_background_light py-4">
+            <div class="container-fluid px-5">
+                <div class="col-lg-12 col-md-12">
+                    <div class="justify-content-center row col-md-12 rounded-3">
+                        <div style="width: 100%; height: 30px; border-bottom: 2px solid white; text-align: center">
+                            <span style="font-size: 40px; background-color:#9FBACD; color: white">
+                               ABOUT US
+                                <!--Padding is optional-->
+                            </span>
+                        </div>
+                        <div class="row box" style="height:300px;">
+                        <h4 style="color: white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h4>
+                        </div>
+
     <!--ANNOUNCEMENT-->
     <section class="flex-sect" id="imagesec">
         <section id="imagesection" class="div_background_light py-4">
@@ -251,7 +267,7 @@
 
                 <!-- <div class="p-2">Vaccination</div> -->
 
-                <a href='login-user.php'>
+                <a href='vaccination.php'>
                 <button type="button" class="btn bg-button"
                     style="background: #FFFFFF; border-radius: 20px; border-width: 10px; padding: 10px;">Vaccination
                     <i class="fa-solid fa-arrow-right" style="color:blue;"></i></button></a>
